726124abd44acb2bbe61097500b7de1c6916f888
Johan Brook <johan.jvb@gmail.com>
2015-03-23 13:54:41 +0100

    Version 1.1.0_1

--- package.js
+++ package.js
@@ -3,7 +3,7 @@ var where = 'client';
 Package.describe({
   name: 'lookback:dropdowns',
   summary: 'Reactive dropdowns for Meteor.',
-  version: '1.1.0',
+  version: '1.1.0_1',
   git: 'http://github.com/lookback/meteor-dropdowns'
 });
 
